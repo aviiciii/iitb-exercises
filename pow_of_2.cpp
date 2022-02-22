dfb05bc362eeb50e3428a2d5c1672c27b39d6c74
aviiciii <88141521+aviiciii@users.noreply.github.com>
2022-02-22 18:50:14 +0530

    Add files via upload

---
+++ pow_of_2.cpp
@@ -0,0 +1,7 @@
+#include <simplecpp>
+
+main_program {
+    for (int i = 1; i <= 65536; i*=2) {
+        cout << i << endl;
+    }
+}
---
+++ square_of_number.cpp
@@ -0,0 +1,13 @@
+#include <simplecpp>
+
+main_program{
+    int i; cin >> i;
+    if (i >= 0) {
+        i*=i;
+        cout << i << endl;
+    }
+    else {
+        i*=i;
+        cout << i << "i" << endl;
+    }
+}
